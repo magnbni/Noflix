0d75928371d30ede2635c1d89333b1234d0befce
sofieothilie <sofieod@stud.ntnu.no>
2023-10-05 12:23:14 +0200

    Implements card for display of movie

---
+++ prosjekt2/src/Components/ActionAreaCard.tsx
@@ -0,0 +1,32 @@
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
+import { Rate } from "./BasicRating";
+import iguana from "../assets/iguana.png";
+
+export default function ActionAreaCard() {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image={iguana}
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Movie Title
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      {Rate(2)}
+    </Card>
+  );
+}
