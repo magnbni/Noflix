80621575197135d5b92cc4abbb73a39f7b36e8f4
jespeste <jespeste@stud.ntnu.no>
2023-11-07 20:08:09 +0100

    Ran prettier. Fixes #24

--- prosjekt2/src/Components/LoginPage.tsx
+++ prosjekt2/src/Components/LoginPage.tsx
@@ -1,26 +1,26 @@
-import * as React from "react"
-import Avatar from "@mui/material/Avatar"
-import Button from "@mui/material/Button"
-import CssBaseline from "@mui/material/CssBaseline"
-import TextField from "@mui/material/TextField"
-import Box from "@mui/material/Box"
-import Typography from "@mui/material/Typography"
-import Container from "@mui/material/Container"
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
 
 // Copied from https://github.com/mui/material-ui/blob/v5.14.17/docs/data/material/getting-started/templates/sign-in/SignIn.tsx
 
 export default function LoginPage() {
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-    const data = new FormData(event.currentTarget)
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
     console.log({
       email: data.get("email"),
       password: data.get("password"),
-    })
-  }
+    });
+  };
 
   return (
-    <Container component='main' maxWidth='xs'>
+    <Container component="main" maxWidth="xs">
       <CssBaseline />
       <Box
         sx={{
@@ -33,42 +33,42 @@ export default function LoginPage() {
         <Avatar sx={{ m: 1, bgcolor: "white" }}>
           <img
             width={30}
-            className='logo'
-            src='https://upload.wikimedia.org/wikipedia/commons/3/3f/Film_reel.svg'
+            className="logo"
+            src="https://upload.wikimedia.org/wikipedia/commons/3/3f/Film_reel.svg"
           />
         </Avatar>
-        <Typography component='h1' variant='h5'>
+        <Typography component="h1" variant="h5">
           Sign in
         </Typography>
-        <Box component='form' onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
           <TextField
-            margin='normal'
+            margin="normal"
             required
             fullWidth
-            id='email'
-            label='Email Address'
-            name='email'
-            autoComplete='email'
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
             autoFocus
           />
           <TextField
-            margin='normal'
+            margin="normal"
             required
             fullWidth
-            name='password'
-            label='Password'
-            type='password'
-            id='password'
-            autoComplete='current-password'
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
           />
           {/* <FormControlLabel
             control={<Checkbox value='remember' color='primary' />}
             label='Remember me'
           /> */}
           <Button
-            type='submit'
+            type="submit"
             fullWidth
-            variant='contained'
+            variant="contained"
             sx={{ mt: 3, mb: 2 }}
           >
             Sign In
@@ -76,5 +76,5 @@ export default function LoginPage() {
         </Box>
       </Box>
     </Container>
-  )
+  );
 }
