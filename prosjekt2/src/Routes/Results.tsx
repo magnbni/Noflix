4d6713abf685977edd3b541a96f88f0ec5e74390
Lars Tønder <larstonder@users.noreply.github.com>
2023-11-08 17:37:59 +0100

    Remove unused code #27

--- prosjekt2/src/Routes/Results.tsx
+++ prosjekt2/src/Routes/Results.tsx
@@ -1,21 +1,9 @@
 import "./Results.css";
 import NestedModal from "../Components/NestedModal";
-import { FilmOptionType, top100Films } from "../types";
 import { useParams } from "react-router-dom";
 import { useState } from "react";
 import HeaderAndDrawer from "../Components/HeaderAndDrawer";
 import { gql, useQuery } from '@apollo/client';
-
-// The main search function. Uses iteration to find the movies, will probably be implemented differently on the backend.
-function search(searchWord: string) {
-  const movies: FilmOptionType[] = [];
-  top100Films.forEach((film) => {
-    if (film.title.toLowerCase().includes(searchWord.toLowerCase())) {
-      movies.push(film);
-    }
-  });
-  return movies;
-}
 
 const GET_MOVIES = gql`
   query {allMovies(first: 10) {
@@ -36,8 +24,6 @@ const GET_MOVIES = gql`
 */
 export default function Results() {
   const { id } = useParams<string>();
-
-  const [movies] = useState(search(id ? id : ""));
 
   const { loading, error, data } = useQuery(GET_MOVIES);
 
