af0d4f4c3cd7491d13ffcf3a597a4ec891ddd35f
sofieothilie <sofieod@stud.ntnu.no>
2023-11-09 00:01:36 +0100

    Emergency bug fix.

--- prosjekt2/src/Components/ComboBox.tsx
+++ prosjekt2/src/Components/ComboBox.tsx
@@ -1,16 +1,45 @@
+import { gql, useQuery } from "@apollo/client";
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
-import { ComboBoxProps, FilmOptionType, top100Films } from "../types";
+import { ComboBoxProps, FilmOptionType } from "../types";
 import React from "react";
+
+const getQuery = (sortOption: string, orderDirection: string) => {
+  let sortValue = "";
+  if (sortOption === "title" && orderDirection === "asc") {
+    sortValue = "TITLE_ASC";
+  } else if (sortOption === "title" && orderDirection === "desc") {
+    sortValue = "TITLE_DESC";
+  } else if (sortOption === "releaseYear" && orderDirection === "asc") {
+    sortValue = "RELEASE_DATE_ASC";
+  } else if (sortOption === "releaseYear" && orderDirection === "desc") {
+    sortValue = "RELEASE_DATE_DESC";
+  }
+
+  return gql`
+    query {
+      allMovies(first: 100, sort: ${sortValue}) {
+        title
+        releaseDate
+        overview
+        voteAverage
+        posterPath
+      }
+    }
+  `;
+};
 
 // The main searchbar of the application, used on the Root homepage
 export default function ComboBox({ onMovieSelect }: ComboBoxProps) {
   const [value, setValue] = React.useState<FilmOptionType | null>(null);
   const [inputValue, setInputValue] = React.useState("");
+  const { data } = useQuery(
+    getQuery("title", "desc"),
+  );
 
   // For the first version we use the top 100 films as default data, taken from the IMDB database
   const defaultProps = {
-    options: top100Films,
+    options: data.allMovies,
     getOptionLabel: (option: FilmOptionType) => option.title,
   };
 
