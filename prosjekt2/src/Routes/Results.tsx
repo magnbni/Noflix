eef24997142639c5a734bab6095fbf5ceb6514b3
sofieothilie <sofieod@stud.ntnu.no>
2023-11-08 23:01:01 +0100

    Ran prettier. Fixes #27

--- prosjekt2/src/Routes/Results.tsx
+++ prosjekt2/src/Routes/Results.tsx
@@ -35,7 +35,7 @@ export default function Results() {
       <div className="row">
         {data.allMovies.map((movie: FilmOptionType) => (
           <div className="card" key={`movie-${movie.title}`}>
-            {NestedModal (movie)}
+            {NestedModal(movie)}
           </div>
         ))}
       </div>
