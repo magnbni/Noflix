ab8e43128c092dcd649e1bd2413a2a26c072c0cd
sofieothilie <sofieod@stud.ntnu.no>
2023-11-16 09:46:06 +0100

    Updates filterandsort.tsx to use the reduxstates. fixes #34

--- prosjekt2/src/Components/FilterAndSort.tsx
+++ prosjekt2/src/Components/FilterAndSort.tsx
@@ -1,9 +1,12 @@
-// import List from "@mui/material/List";
-// import ListItem from "@mui/material/ListItem";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
 // import { FormControlLabel, Slider, Switch } from "@mui/material";
-// import { MovieType, top100Films } from "../types";
-// import { useState } from "react";
-// import "./FilterAndSort.css";
+import { FormControlLabel, Switch } from "@mui/material";
+// import { MovieType } from "../types";
+import "./FilterAndSort.css";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../app/store";
+import { sortByAsc, sortByTitle } from "../Reducers/SortSlice";
 
 // function valuetext(value: number) {
 //   return `${value}`;
@@ -59,70 +62,71 @@
 //   return marks;
 // }
 
-// export default function FilterAndSort() {
-//   const [sortByYear, setSortByYear] = useState(false);
-//   const [sortByTitle, setSortByTitle] = useState(false);
-//   const [range, setRange] = useState<number[]>([
-//     findYearLimits(movies).lowestYear!,
-//     findYearLimits(movies).highestYear!,
-//   ]);
-//   const marks = createMarks(movies);
+export default function FilterAndSort() {
+  const dispatch = useDispatch();
+  const sortAscState = useSelector(
+    (state: RootState) => state.sort.sortAsc
+  );
+  const sortByTitleState = useSelector(
+    (state: RootState) => state.sort.sortByTitle
+  );
 
-//   const updateSortByYear = () => {
-//     setSortByYear(!sortByYear);
-//   };
+  const updateSortAsc = () => {
+    dispatch(sortByAsc(!sortAscState));
+  };
 
-//   const updateSortByTitle = () => {
-//     setSortByTitle(!sortByTitle);
-//   };
+  const updateSortByTitle = () => {
+   dispatch(sortByTitle(!sortByTitleState));
+  };
 
 //   const updateFilterByYear = (_event: Event, newRange: number | number[]) => {
 //     const newRangeArray: number[] = newRange as number[];
 //     setRange([newRangeArray[0], newRangeArray[1]]);
 //   };
 
-//   return (
-//     <List className="list">
-//       <ListItem key="sortbyyear" disablePadding>
-//         <FormControlLabel
-//           control={
-//             <Switch
-//               defaultChecked
-//               onChange={updateSortByYear}
-//               color="default"
-//             />
-//           }
-//           label="Sort by year"
-//           className="switch"
-//         />
-//       </ListItem>
-//       <ListItem key="sortbytitle" disablePadding>
-//         <FormControlLabel
-//           control={
-//             <Switch
-//               defaultChecked
-//               onChange={updateSortByTitle}
-//               color="default"
-//             />
-//           }
-//           label="Sort by title"
-//           className="switch"
-//         />
-//       </ListItem>
-//       <ListItem key="rangeyear">
-//         <Slider
-//           getAriaLabel={() => "Release year range"}
-//           value={[range[0], range[1]]}
-//           onChange={updateFilterByYear}
-//           valueLabelDisplay="auto"
-//           getAriaValueText={valuetext}
-//           marks={marks}
-//           min={marks[0].value}
-//           step={1}
-//           max={marks[marks.length - 1].value}
-//           className="slider"
-//         />
-//       </ListItem>
-//     </List>
-//   );
-// }
+  return (
+    <List className="list">
+      <ListItem key="sortAsc" disablePadding>
+        <FormControlLabel
+          control={
+            <Switch
+              defaultChecked
+              onChange={updateSortAsc}
+              color="default"
+            />
+          }
+          label={sortAscState ? "Ascending" : "Descending"}
+          className="switch"
+        />
+      </ListItem>
+      <ListItem key="sortbytitle" disablePadding>
+        <FormControlLabel
+          control={
+            <Switch
+              defaultChecked
+              onChange={updateSortByTitle}
+              color="default"
+            />
+          }
+          label={sortByTitleState ? "Title" : "Year"}
+
+          className="switch"
+        />
+      </ListItem>
+      {/* <ListItem key="rangeyear">
+        <Slider
+          getAriaLabel={() => "Release year range"}
+          value={[range[0], range[1]]}
+          onChange={updateFilterByYear}
+          valueLabelDisplay="auto"
+          getAriaValueText={valuetext}
+          marks={marks}
+          min={marks[0].value}
+          step={1}
+          max={marks[marks.length - 1].value}
+          className="slider"
+        />
+      </ListItem> */}
+    </List>
+  );
+}
