372127bfd7f349a1aef5d16fc041c302c7e076b9
Lars Tønder <larstonder@users.noreply.github.com>
2023-11-18 17:09:45 +0100

    add hasNextPage and hasPreviousPage arguments to PageInfo #36

--- backend/schema.py
+++ backend/schema.py
@@ -1,4 +1,5 @@
 import base64
+import math
 import graphene
 import bcrypt
 from graphene.relay import Node
@@ -15,7 +16,7 @@ class Movie(MongoengineObjectType):
         model = MovieModel
         interfaces = (Node,)
         filter_fields = {
-            "title": ["exact", "iexact", "contains", "icontains"],  # Add regex options
+            "title": ["exact", "iexact", "contains", "icontains"],
             "release_date": ["exact", "icontains", "istartswith"],
         }
         ordering = "title"
@@ -27,15 +28,47 @@ class MovieEdge(graphene.ObjectType):
     def resolve_cursor(self, info):
         return base64.b64encode(str(self.node._id).encode('utf-8')).decode('utf-8')
 
+class PageInfo(graphene.ObjectType):
+    has_next_page = graphene.Boolean()
+    has_previous_page = graphene.Boolean()
+
+def get_total_count_of_movies():
+    return MovieModel.objects.count()
+
 class MovieConnection(graphene.relay.Connection):
     class Meta:
         node = Movie
 
     edges = graphene.List(MovieEdge)
+    page_info = graphene.Field(PageInfo)
 
     def resolve_edges(self, info):
         edges = [MovieEdge(node=movie) for movie in self.iterable]
         return edges
+    
+    def resolve_page_info(self, info):
+        args = info.context['all_movies_args']
+        print(args)
+        first = args.get('first', 0)
+        after = args.get('after')
+
+        start = 0
+        last_movie_id = None
+        if after is not None:
+            last_movie_id = ObjectId(base64.b64decode(after).decode('utf-8'))
+
+        total_count = get_total_count_of_movies()
+
+        has_next_page = False
+        if last_movie_id is not None:
+            remaining_movies = MovieModel.objects.filter(_id__gt=last_movie_id)
+            has_next_page = remaining_movies.count() > first
+        else:
+            has_next_page = total_count > first
+        
+        has_previous_page = last_movie_id is not None
+
+        return PageInfo(has_next_page=has_next_page, has_previous_page=has_previous_page)
 
 class User(MongoengineObjectType):
     class Meta:
@@ -105,9 +138,18 @@ class Query(graphene.ObjectType):
     )
 
     def resolve_all_movies(
-        self, info, sort=None, first=None, title=None, release_date=None, after=None, offset=None
+        self, info, **args
     ):
         query = MovieModel.objects.all()
+        
+        info.context['all_movies_args'] = args
+
+        sort= args.get('sort')
+        title = args.get('title')
+        release_date = args.get('release_date')
+        offset = args.get('offset')
+        first = args.get('first')
+        after = args.get('after')
 
         if sort == "title_asc":
             query = query.order_by("title")
@@ -137,11 +179,13 @@ class Query(graphene.ObjectType):
             query = query[:first]
         return query
 
-
 class Mutation(graphene.ObjectType):
     node = Node.Field()
     auth_user = AuthenticateUser.Field()
     user_create = CreateUser.Field()
 
+if __name__ == "__main__":
+    id = ObjectId("654b65d6671ff470fcdd7bfe")
+    print(base64.b64encode(str(id).encode('utf-8')).decode('utf-8'))
 
 schema = graphene.Schema(query=Query, mutation=Mutation, types=[Movie, User])
