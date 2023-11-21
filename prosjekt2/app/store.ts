0c9b2f2205195ca4e2feac9e1e33ba22f714b98e
sofieothilie <sofieod@stud.ntnu.no>
2023-11-21 13:39:15 +0100

    Prettier. Fixes #54

--- prosjekt2/app/store.ts
+++ prosjekt2/app/store.ts
@@ -11,18 +11,25 @@ export const store = configureStore({
   },
   preloadedState: {
     user: {
-      authUser : localStorageState.getItem("authUser") ? JSON.parse(localStorageState.getItem("authUser")!) : false,
-      email : localStorageState.getItem("email") ? JSON.parse(localStorageState.getItem("email")!) : "",
+      authUser: localStorageState.getItem("authUser")
+        ? JSON.parse(localStorageState.getItem("authUser")!)
+        : false,
+      email: localStorageState.getItem("email")
+        ? JSON.parse(localStorageState.getItem("email")!)
+        : "",
     },
   },
-  }
-);
-
+});
 
 store.subscribe(() => {
-  localStorageState.setItem("authUser", JSON.stringify(store.getState().user.authUser));
-  localStorageState.setItem("email", JSON.stringify(store.getState().user.email));
-
+  localStorageState.setItem(
+    "authUser",
+    JSON.stringify(store.getState().user.authUser),
+  );
+  localStorageState.setItem(
+    "email",
+    JSON.stringify(store.getState().user.email),
+  );
 });
 
 export type RootState = ReturnType<typeof store.getState>;
