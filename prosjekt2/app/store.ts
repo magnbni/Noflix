1bee79acb00e31ae8ec2a16b271d45ee356c4e4c
sofieothilie <sofieod@stud.ntnu.no>
2023-11-21 13:38:28 +0100

    Links store to localStorage for user. Fixes #54

--- prosjekt2/app/store.ts
+++ prosjekt2/app/store.ts
@@ -2,11 +2,27 @@ import { configureStore } from "@reduxjs/toolkit";
 import { sortSlice } from "../src/Reducers/SortSlice";
 import { userSlice } from "../src/Reducers/UserSlice";
 
+const localStorageState = window.localStorage;
+
 export const store = configureStore({
   reducer: {
     sort: sortSlice.reducer,
     user: userSlice.reducer,
   },
+  preloadedState: {
+    user: {
+      authUser : localStorageState.getItem("authUser") ? JSON.parse(localStorageState.getItem("authUser")!) : false,
+      email : localStorageState.getItem("email") ? JSON.parse(localStorageState.getItem("email")!) : "",
+    },
+  },
+  }
+);
+
+
+store.subscribe(() => {
+  localStorageState.setItem("authUser", JSON.stringify(store.getState().user.authUser));
+  localStorageState.setItem("email", JSON.stringify(store.getState().user.email));
+
 });
 
 export type RootState = ReturnType<typeof store.getState>;
