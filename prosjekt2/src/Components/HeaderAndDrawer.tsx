507f9a4ff7c8e0ca9cdd2c8aaaa0dfdf5a86f97c
sofieothilie <sofieod@stud.ntnu.no>
2023-11-21 21:09:40 +0100

    Turns off autocomplete as this is irrelevant information. Fixes #57

--- prosjekt2/src/Components/HeaderAndDrawer.tsx
+++ prosjekt2/src/Components/HeaderAndDrawer.tsx
@@ -117,6 +117,7 @@ export default function HeaderAndDrawer() {
                 fullWidth
                 onChange={handleSearchChange}
                 onKeyDown={handleKeyPress}
+                autoComplete='off'
               />
             </div>
           )}
