af7b790b27b0e0767d345491c47884d5b912ae81
Magnus Brekke Nilsen <magnus.b.nilsen@ntnu.no>
2023-11-22 13:51:13 +0100

    Set up simple test for search Fixes #44

--- prosjekt2/cypress/e2e/spec.cy.ts
+++ prosjekt2/cypress/e2e/spec.cy.ts
@@ -1,5 +1,7 @@
-describe('My First Test', () => {
-  it('Does not do much!', () => {
-    expect(true).to.equal(true)
+describe('Test Search', () => {
+  it('Visits Noflix', () => {
+    cy.visit('/')
+    cy.get('.searchHeader').type('lord of the rings{enter}')
+    cy.url().should('include', '/search/lord%20of%20the%20rings')
   })
 })
